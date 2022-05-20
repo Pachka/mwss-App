4ca6b02cf9ae2d3fc017b6ae78feb897918b6165
Pachka <46706800+Pachka@users.noreply.github.com>
2022-05-20 16:39:01 +0200

    Update about.R

--- body/about.R
+++ body/about.R
@@ -8,31 +8,29 @@ tabItemAbout <- function(){
 
   tabItem("ABOUT",
           markdown("
-    # About
-
+# About
 ## mwss-App: an R-Shiny application to run stochastic simulation of infectious diseases spreading in healthcare systems structured as networked metapopulations
 
 ### Authors
-<font size="-2">
-Hammami Pachka<sup>1,2,3,*</sup>, Oodally Ajmal<sup>1,2,3,*</sup>, Reilhac Astrid<sup>4</sup>, Guérineau de Lamérie Guillaume<sup>4</sup>,  Widgren Stefan <sup>5</sup>,  Temime Laura<sup>3,6,¤</sup> and  Opatwoski Lulla<sup>1,2,¤</sup></font>
+Hammami Pachka<sup>1,2,3,&#128312;</sup>, Oodally Ajmal<sup>1,2,3,&#128312;</sup>, Reilhac Astrid<sup>4</sup>, Gu&eacute;rineau de Lam&eacute;rie Guillaume<sup>4</sup>,  Widgren Stefan <sup>5</sup>,  Temime Laura<sup>3,6,&#10070;</sup> and  Opatwoski Lulla<sup>1,2,&#10070;</sup></font>
 
 <br>
-<sup>1</sup> Anti-infective evasion and pharmacoepidemiology team, Université Paris-Saclay, UVSQ, Inserm, CESP,  Montigny-Le-Bretonneux, France
+<sup>1</sup> Anti-infective evasion and pharmacoepidemiology team, Universit&eacute; Paris-Saclay, UVSQ, Inserm, CESP,  Montigny-Le-Bretonneux, France
 
 <sup>2</sup> Epidemiology and Modelling of Antibiotic Evasion (EMAE), Institut Pasteur, Paris, France
 
-<sup>3</sup> Laboratoire de Modélisation,  épidémiologie et surveillance des risques sanitaires (MESuRS), Conservatoire national des arts et métiers, Paris, France
+<sup>3</sup> Laboratoire de Mod&eacute;lisation,  &eacute;pid&eacute;miologie et surveillance des risques sanitaires (MESuRS), Conservatoire national des arts et m&eacute;tiers, Paris, France
 
-<sup>4</sup> Département d'information médicale, Centre hospitalier Guillaume Régnier, Rennes, France
+<sup>4</sup> D&eacute;partement d'information m&eacute;dicale, Centre hospitalier Guillaume R&eacute;gnier, Rennes, France
 
 <sup>5</sup> Department of Disease Control and Epidemiology, National Veterinary Institute, Uppsala, Sweden
 
-<sup>6</sup> PACRI unit, Institut Pasteur, Conservatoire national des arts et métiers, Paris, France
+<sup>6</sup> PACRI unit, Institut Pasteur, Conservatoire national des arts et m&eacute;tiers, Paris, France
 <sup>7</sup>MRC Centre for Global Infectious Disease Analysis, Department of Infectious Disease Epidemiology, Imperial College London, United Kingdom
 
-<sup>*</sup>These authors contributed equally
+<sup>&#128312;</sup>These authors contributed equally
 
-<sup>¤</sup>These authors contributed equally
+<sup>&#10070;</sup>These authors contributed equally
 
 </br>
 
