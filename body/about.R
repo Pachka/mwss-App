d815adfda9cc4a18e442653354e6a1fdcef3912e
Pachka <46706800+Pachka@users.noreply.github.com>
2022-05-20 16:31:25 +0200

    Update about.R

--- body/about.R
+++ body/about.R
@@ -10,18 +10,35 @@ tabItemAbout <- function(){
           markdown("
     # About
 
-    #### COVID-19 DISEASE NOSOCOMIAL SPREAD IN LONG-TERM CARE SETTINGS (Beta 0.1 Version)
+## mwss-App: an R-Shiny application to run stochastic simulation of infectious diseases spreading in healthcare systems structured as networked metapopulations
 
     ### Authors
-    Hammami Pachka (1,2), Oodally Ajmal (1,2), Laura Temime (3), Lulla Opatowski (2)
+<font size="-2">
+   Hammami Pachka<sup>1,2,3,*</sup>, Oodally Ajmal<sup>1,2,3,*</sup>, Reilhac Astrid<sup>4</sup>, Guérineau de Lamérie Guillaume<sup>4</sup>,  Widgren Stefan <sup>5</sup>,  Temime Laura<sup>3,6,¤</sup> and  Opatwoski Lulla<sup>1,2,¤</sup></font>
 
-    1. Universite de Versailles Saint-Quentin-en-Yvelines, membre associe de l'Universite Paris-Saclay
-    2. Institut Pasteur
-    3. Conservatoire national des arts et metiers
+<br>
+<sup>1</sup> Anti-infective evasion and pharmacoepidemiology team, Université Paris-Saclay, UVSQ, Inserm, CESP,  Montigny-Le-Bretonneux, France
+
+<sup>2</sup> Epidemiology and Modelling of Antibiotic Evasion (EMAE), Institut Pasteur, Paris, France
+
+<sup>3</sup> Laboratoire de Modélisation,  épidémiologie et surveillance des risques sanitaires (MESuRS), Conservatoire national des arts et métiers, Paris, France
+
+<sup>4</sup> Département d'information médicale, Centre hospitalier Guillaume Régnier, Rennes, France
+
+<sup>5</sup> Department of Disease Control and Epidemiology, National Veterinary Institute, Uppsala, Sweden
+
+<sup>6</sup> PACRI unit, Institut Pasteur, Conservatoire national des arts et métiers, Paris, France
+<sup>7</sup>MRC Centre for Global Infectious Disease Analysis, Department of Infectious Disease Epidemiology, Imperial College London, United Kingdom
+
+<sup>*</sup>These authors contributed equally
+
+<sup>¤</sup>These authors contributed equally
+
+</br>
 
 
     ### Contact:
-    email@adress.fr
+Corresponding author: Hammami Pachka (pachka@hotmail.fr)
                    ")
   )
 }
