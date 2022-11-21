4ad93c5d392c352b8668a12d6bb6d47e4b8351ad
U117-S829 <pachka@hotmail.fr>
2022-11-21 14:40:06 +0100

    remove tmp params writting

--- functions/synthreportModule.R
+++ functions/synthreportModule.R
@@ -28,9 +28,9 @@ synthreport <- function(input, output, session, model, variable, n_days, n_sim) 
                          data = variable(),
                          n_days = n_days(),
                          n_sim = n_sim())
-str(params)
-print(params)
-        save(params, file = "paramsReport.rda")
+        # str(params)
+        # print(params)
+        # save(params, file = "paramsReport.rda")
 
         # Knit the document, passing in the `params` list, and eval it in a
         # child of the global environment (this isolates the code in the document
@@ -42,35 +42,6 @@ print(params)
         )
     }
   
-    # filename = function() {
-    #   paste("MWSS_report-", Sys.Date(), ".pdf", sep = "")
-    # },
-    # content = function(file) {
-    # 
-    #   # Copy the report file to a temporary directory before processing it, in
-    #   # case we don't have write permissions to the current working dir (which
-    #   # can happen when deployed).
-    #   tempReport <- file.path(tempdir(), "report.Rmd")
-    #   file.copy("report.Rmd", tempReport, overwrite = TRUE)
-    # 
-    #   # Set up parameters to pass to Rmd document
-    #     params <- list(control_traj = model(),
-    #                    data = variable(),
-    #                    n_days = n_days(),
-    #                    n_sim = n_sim())
-    # 
-    #   # save(params, file = "paramsReport.rda")
-    # 
-    #   # Knit the document, passing in the `params` list, and eval it in a
-    #   # child of the global environment (this isolates the code in the document
-    #   # from the code in this app).
-    #   rmarkdown::render(tempReport,
-    #                     output_file = file,
-    #                     params = params,
-    #                     envir = new.env(parent = globalenv())
-    #   )
-    #     
-    # }
   )
 
     }
